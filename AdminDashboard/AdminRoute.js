6a9d6021e3401d3dc9423cbcf83d355a350a57c3
ali zamani <azamani081@gmail.com>
2025-11-17 16:42:03 +0330

    Get product information from the dashboard page

--- AdminDashboard/AdminRoute.js
+++ AdminDashboard/AdminRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const AdminRoute = express.Router();
 
-AdminRoute.get("/",(req,res)=>{
+AdminRoute.get("/Dashboard",(req,res)=>{
         res.render("AdminDashboard")
 })
 
